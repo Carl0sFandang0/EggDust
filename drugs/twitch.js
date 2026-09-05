0ce17b78717094b76cb46a8f9e5f10ce048c7061
Carl0sFandang0 <cfxmeigh@hotmail.com>
2026-09-05 10:45:43 +0100

    Add files via upload

--- drugs/twitch.js
+++ drugs/twitch.js
@@ -3,14 +3,18 @@ const elements = document.querySelectorAll('.random-twitch');
 function startRandomLoop(element) {
     const min = parseInt(element.getAttribute('data-min')) || 500;
     const max = parseInt(element.getAttribute('data-max')) || 3000;
+
     function shake() {
         element.classList.add('shake-once');
 
-        element.addEventListener('animationend', () => { element.classList.remove('shake-once'); }, { once: true });
+        element.addEventListener('animationend', () => {
+            element.classList.remove('shake-once');
+        }, { once: true });
 
         const nextDelay = Math.random() * (max - min) + min;
 
         setTimeout(shake, nextDelay);
+
     }
 
     shake();
