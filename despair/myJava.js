fa40568634d9efbd76a58e5d21ba85b8a1a8d270
Carl0sFandang0 <cfxmeigh@hotmail.com>
2026-09-13 16:44:18 +0100

    Add files via upload

--- despair/myJava.js
+++ despair/myJava.js
@@ -1,20 +1,53 @@
 const filmstrip = document.getElementById('filmstrip');
-const frameWidth = 853.6 ; // Width of one frame in pixels
+const frameWidth = 630 ; // Width of one frame in pixels
 const totalFrames = 100; // Total images side-by-side
+const endloopFrame = 80; // endloop cycles between here and totalFrames
 let currentFrame = 0;
 let animationTimer = null;
+let endLoop = 0;
+let xPosition = 0;
 
-function nextFrame() { currentFrame++;
-    if (currentFrame >= totalFrames) { currentFrame = 0; }
-    const xPosition = -(currentFrame * frameWidth);
-    filmstrip.style.transform = `translateX(${xPosition}px)`;
+function nextFrame() {
+    if (endLoop == 1) {
+        currentFrame--;
+        if (currentFrame <= endloopFrame) {
+            currentFrame++;
+            endLoop = 2;
+        }
+        else {
+            xPosition = -(currentFrame * frameWidth);
+            filmstrip.style.transform = `translateX(${xPosition}px)`;
+        }
+    }
+    else if (endLoop == 2) {
+        currentFrame++;
+        if (currentFrame >= totalFrames) {
+            currentFrame--;
+            endLoop = 1;
+        }
+        else {
+            xPosition = -(currentFrame * frameWidth);
+            filmstrip.style.transform = `translateX(${xPosition}px)`;
+        }
+    }
+    else { 
+        currentFrame++;
+        if (currentFrame >= totalFrames) {
+            currentFrame--;
+            endLoop = 1;
+        }
+        else {
+            xPosition = -(currentFrame * frameWidth);
+            filmstrip.style.transform = `translateX(${xPosition}px)`;
+        }
+    }
 }
 
 function startAnimation() {
     // Prevent duplicate intervals if clicked multiple times 
     if (animationTimer) clearInterval(animationTimer);
     // Run at 5 frames per second
-    animationTimer = setInterval(nextFrame, 200);
+    animationTimer = setInterval(nextFrame, 300);
 }
 function stopAnimation() { clearInterval(animationTimer); }
 
